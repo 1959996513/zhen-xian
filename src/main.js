2c552d85738c7165e03b63186dafc14a9354a2cb
1959996513 <3361982986@qq.com>
2018-11-10 13:29:13 +0800

    首页完成

--- src/main.js
+++ src/main.js
@@ -5,11 +5,18 @@ import App from './App'
 import router from './router'
 import Vant from 'vant';
 import 'vant/lib/index.css';
-
+//将图标库中js文件引入
+import '@/assets/icon/iconfont'
+import '@/style/index.scss'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 
 Vue.config.productionTip = true
 Vue.use(Vant,{
+
+})
+Vue.use(ElementUI,{
 
 })
 /* eslint-disable no-new */
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -5,25 +5,28 @@
      <van-row type="flex" class="hander" justify="space-around"   align='center'>
        <van-col span="6" style='padding-left: 30px'>
          <a href="">
-           <span style="color:#ffffff;font-size: 24px;">郑州</span>
-           <img src="../img/定位选择按钮@2x.png" alt="" style="width: 18px; height: 9px;">
+           <span style=" color: #999999;font-size: 24px;">郑州</span>
+           <img src="../img/xiala.png" alt="" style="width: 18px; height: 9px;">
          </a>
        </van-col>
-       <van-col span="6" style=" width:420px; position: relative;">
-         <input type="text"  style=" width: 420px; height: 52px; border-radius: 30px;border: 0;">
-         <div style="position: absolute; top: 10px;height: 28px;left: 130px;">
-         <img src="../img/搜索按钮@2x.png" alt="" style="height: 28px;">
-         <span style="color: #999999;font-size: 24px;line-height: 28px;">搜索商品</span>
-         </div>
-       </van-col>
+
+
+           <van-field
+             label="搜索商品"
+             left-icon="find"
+             span="6" style="display: flex;align-items: center; width:420px;height: 52px; border: 1px solid #222222;border-radius: 15px;padding-left: 80px;font-size: 20px;color: #999999;"
+
+           />
+
+
        <van-col span="6" style="margin-right: 32px;">
          <a href="" style="float: right;">
-           <img src="../img/xiaoxi.png" alt="" style="width: 26px; height: 26px;">
+           <img src="../img/xiaoxi1.png" alt="" style="width: 26px; height: 26px;">
          </a>
        </van-col>
      </van-row>
 
-     <van-swipe :autoplay="3000" class="lun">
+     <van-swipe :autoplay="3000" class="lun" style="padding-top: 88px;">
        <van-swipe-item><img src="../img/baaner@2x.png" alt=""></van-swipe-item>
        <van-swipe-item><img src="../img/baaner@2x.png" alt=""></van-swipe-item>
        <van-swipe-item><img src="../img/baaner@2x.png" alt=""></van-swipe-item>
@@ -55,15 +58,21 @@
        <van-col style="font-size: 20px;font-family: PingFang-SC-Medium;color: #666666;margin:20px 0 30px 0 ">New Arrivals</van-col>
      </van-row>
      <!--轮播-->
-     <van-swipe style="margin-left: 30px" >
-       <van-swipe-item style="width: 530px;height: 300px;margin-right: 20px">
-         <img src="../assets/新品推荐大图@2x.png" alt="">
-       </van-swipe-item>
-       <van-swipe-item style="height: 258px;margin-top: 21px;">
-         <img src="../assets/新品推荐小图@2x.png" alt="" height=258px>
-       </van-swipe-item>
-       <div class="custom-indicator" slot="indicator"></div>
-     </van-swipe>
+     <el-carousel  type="card" height="270px">
+       <el-carousel-item>
+         <img src="../assets/新品推荐大图@2x.png" alt="" width=370 height=250>
+       </el-carousel-item>
+       <el-carousel-item>
+         <img src="../assets/新品推荐小图@2x.png" alt="" width=370 height=250>
+       </el-carousel-item>
+       <el-carousel-item>
+         <img src="../assets/图1.png" alt="" width=370 height=250>
+       </el-carousel-item>
+       <el-carousel-item>
+         <img src="../assets/图2.png" alt="" width=370 height=250>
+       </el-carousel-item>
+     </el-carousel>
+
      <div style="width: 750px;height: 20px;background-color: #f2f2f2;margin-top: 50px"></div>
 
      <!--拼团推荐-->
@@ -105,6 +114,79 @@
        </van-col>
      </van-row>
      <div style="width: 750px;height: 20px;background-color: #f2f2f2;margin-top: 50px"></div>
+
+     <!--特惠推荐-->
+
+     <van-row type="flex" justify="center" style="padding-top: 40px;">
+       <van-col style="width: 80px;height: 1px;background-color: #222222;margin: auto 0"></van-col>
+       <van-col style="font-size: 32px;font-family:'粗体';color: #222222;margin: 0 9px">特惠推荐</van-col>
+       <van-col style="width: 80px;height: 1px;background-color: #222222;margin: auto 0"></van-col>
+     </van-row>
+
+     <van-row type="flex" justify="center">
+       <van-col style="font-size: 20px;font-family: PingFang-SC-Medium;color: #666666;margin:20px 0 29px 0 ">Collage recommendation</van-col>
+     </van-row>
+     <van-row type="flex" style=" height:450px ;">
+       <van-col style="box-shadow: 5px 6px 10px #f2f2f2;border-radius: 11px;">
+         <img src="../img/特惠推荐1@2x.png" class="tehuitu1" alt="">
+         <p class="p1">巴黎情人甜点</p>
+         <p class="p2">￥ 88.00</p>
+       </van-col>
+       <van-col style="box-shadow: 5px 6px 10px #f2f2f2; height:450px;border-radius: 11px;">
+         <img src="../img/特惠专区图2@2x.png" class="tehuitu2" alt="">
+         <p class="p3">荷兰卡尔瓦多斯</p>
+         <p class="p4">￥ 290.00</p>
+       </van-col>
+     </van-row>
+
+     <van-row type="flex" >
+       <van-col style="box-shadow: 5px 6px 10px #f2f2f2;margin-top: 30px; margin-bottom: 120px;height:450px;border-radius: 11px;">
+         <img src="../img/特惠专区图3@2x.png" class="tehuitu1" alt="">
+         <p class="p1">西班牙Oroblanco</p>
+         <p class="p2">￥ 120.00</p>
+       </van-col>
+       <van-col style="box-shadow: 5px 6px 10px #f2f2f2;margin-top: 30px; margin-bottom: 120px;height:450px;border-radius: 11px;">
+         <img src="../img/特惠专区图4@2x.png" class="tehuitu2" alt="">
+         <p class="p3">云南蒙自黑加仑</p>
+         <p class="p4">￥ 50.00</p>
+       </van-col>
+     </van-row>
+     <!--尾部-->
+     <van-tabbar style="height:98px;margin-top:30px;z-index: 9;">
+       <van-tabbar-item style="text-align:left;padding-left: 72px;">
+
+         <img class="image" src="../img/首页点击时@2x.png"  />
+         <p class="p">首页</p>
+       </van-tabbar-item>
+
+       <!--2-->
+
+       <van-tabbar-item >
+
+         <img class="image" src="../img/发现未点击时@2x.png"
+         >
+         <p class="p">发现</p>
+       </van-tabbar-item>
+
+       <!--3-->
+
+       <van-tabbar-item >
+
+         <img class="image"
+              src="../img/购物车未点击时@2x.png"
+         >
+         <p class="p">购物车</p>
+       </van-tabbar-item>
+       <!--4-->
+
+       <van-tabbar-item style="text-align:right;padding-right:72px;">
+
+         <img class="image" src="../img/我的未点击时@2x.png" />
+         <p class="p">我的</p>
+       </van-tabbar-item>
+
+     </van-tabbar>
+
    </div>
 </template>
 
@@ -135,12 +217,11 @@
   }
 
   .hander{
-  background-color: rgba(255,255,255,0);
+  background-color: rgb(255,255,255);
 
   height: 88px;
   font-size: 24px;
-
-  color: #ffffff;
+  color: #999999;
   position: fixed;
   width: 100%;
   overflow: hidden;
@@ -153,4 +234,81 @@
     width: 100%;
     height: 408px;
   }
+
+  .p1{
+    font-family: "PingFang SC";
+    margin-left: 60px;
+    margin-top: 30px;
+    margin-bottom: 20px;
+    font-size: 28px;
+    color: #222222;
+    height: 24px;
+  }
+
+  .p2{
+    margin-top: 0;
+    font-family: "PingFang SC";
+    margin-left: 60px;
+   padding-bottom: 30px;
+    font-size: 28px;
+    color: #ff9232;
+    height: 24px;
+  }
+  .p3{
+    font-family: "PingFang SC";
+    margin-left:60px ;
+    margin-top: 30px;
+    margin-bottom: 20px;
+    font-size: 28px;
+    color: #222222;
+    height: 24px;
+  }
+  .p4{
+    margin-top: 0;
+    font-family: "PingFang SC";
+    margin-left: 60px;
+    margin-bottom: 30px;
+    font-size: 28px;
+    color: #ff9232;
+    height: 24px;
+  }
+  .yinying{
+    width: 100%;
+    height: 20px;
+
+  }
+  .tehuitu1{
+    width:335px;
+    height: 322px;
+    margin-left: 30px;
+  }
+
+  .tehuitu2{
+    width:335px;
+    height: 322px;
+    margin-left: 20px;
+  }
+
+  .p{
+
+    margin-top: 8px;
+    margin-bottom: 12px;
+    font-family: "PingFang SC";
+    font-size: 20px;
+    height: 20px;
+    line-height: 20px;
+
+  }
+  .image{
+    width: 44px;
+    height: 44px;
+    margin-top: 14px;
+  }
+  .el-carousel__item{
+    width: 370px;
+    height: 250px;
+    margin: 0;
+    box-shadow: 0 6px 10px #f2f2f2;
+  }
+
 </style>
